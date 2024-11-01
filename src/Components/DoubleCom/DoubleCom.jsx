62b9487459cd9098acc56e13c464c4e72123e73e
sairam-jeeva <sairamjeeva18@gmail.com>
2024-11-01 12:26:15 +0530

    final

--- src/Components/DoubleCom/DoubleCom.jsx
+++ src/Components/DoubleCom/DoubleCom.jsx
@@ -28,7 +28,7 @@ const DoubleCom = () => {
             Wide Range in All Types of Stationary Items
           </h1>
           <p className='mt-3 text-[14px] md:text-[16px] xl:text-[18px] text-gray-800'>
-            Krish Stationary provides wide range of Stationary products in all kinds of industries and for wide range of peoples
+            Krish Stationery provides wide range of Stationary products in all kinds of industries and for wide range of peoples
           </p>
           <a href="#ourProducts" className='mt-5'>
             <button className='bg-purple-900 text-white hover:bg-purple-700 h-[40px] w-[120px] md:h-[50px] md:w-[150px] rounded-md'>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -34,21 +34,20 @@ const Header = () => {
       />
 
       <ul
-        className={`flex flex-col md:flex-row items-center gap-4 md:gap-8 transition-all duration-300 
+        className={`flex flex-col md:flex-row items-center gap-4 md:gap-8 transition-all duration-100 
         ${isOpen ? 'absolute top-0 left-0 w-full bg-slate-500 text-white h-screen flex flex-col items-center justify-center md:static md:bg-transparent' : 'hidden md:flex'}`}
       >
-        <div className={`absolute top-0 left-0 w-full h-full bg-black opacity-50 ${isOpen ? 'block' : 'hidden'} transition-opacity duration-300`} onClick={toggleMenu}></div>
-        
-        <li className={`text-xl md:text-lg py-2 md:py-0 transition-colors duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
+        <div className={`absolute top-0 left-0 w-full h-full bg-black opacity-50 ${isOpen ? 'block' : 'hidden'} transition-opacity duration-300`} onClick={toggleMenu}></div>  
+        <li className={`text-xl md:text-lg py-2 md:py-0  duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
           <a href="#krishImage" onClick={() => setIsOpen(false)}>Stationery</a>
         </li>
-        <li className={`text-xl md:text-lg py-2 md:py-0 transition-colors duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
+        <li className={`text-xl md:text-lg py-2 md:py-0 duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
           <a href="#krish_electricals" onClick={() => setIsOpen(false)}>Electricals</a>
         </li>
-        <li className={`text-xl md:text-lg py-2 md:py-0 transition-colors duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
+        <li className={`text-xl md:text-lg py-2 md:py-0 duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
           <a href="#additionals" onClick={() => setIsOpen(false)}>Additional </a>
         </li>
-        <li className={`text-xl md:text-lg py-2 md:py-0 transition-colors duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
+        <li className={`text-xl md:text-lg py-2 md:py-0 duration-200 ${isOpen ? 'text-white' : isScrolled ? 'text-white' : 'text-gray-600'}`}>
           <a href="#premiumDis" onClick={() => setIsOpen(false)}>Special Combo</a>
         </li>
         <li className="mt-4 md:mt-0">
